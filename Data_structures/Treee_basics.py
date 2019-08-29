9b9412847240a9ee67cb22e93bb3921ef7180401
arekgitrepo <anudeepr44@gmail.com>
2019-08-28 20:29:19 -0700

    corrected method

--- Data_structures/Treee_basics.py
+++ Data_structures/Treee_basics.py
@@ -149,7 +149,7 @@ class BinTreee(object):
         list_of_nodes = []
         traversal_queue = deque([self.root_node])
         while len(traversal_queue) > 0:
-            node=traversal_queue.pop()
+            node=traversal_queue.popleft()
             list_of_nodes.append(node)
             if node.left_child:
                 traversal_queue.append(node.left_child)
